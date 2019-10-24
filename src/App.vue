8363e19740384f20a403b19ae1394ec5a6a85e04
Laurie Frank <laurie360@hotmail.co.uk>
2019-10-24 11:29:37 +0100

    creates app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Energy Tracker</h1>
+    <energy-tracker :energyTypes="energyTypes"></energy-tracker>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EnergyTracker from './components/EnergyTracker.vue'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      "data":[
+      "from": "2018-01-20T12:00Z",
+      "to": "2018-01-20T12:30Z",
+      energyTypes: [
+        {"fuel": "Milk", "perc": 0.1},
+        {"fuel": 'Cheese', "perc": 2.2},
+        {"fuel": 'Beans', "perc": 17.9},
+      ],
+    }]
+  },
   components: {
-    HelloWorld
-  }
+    "energy-tracker": EnergyTracker
+  },
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
-
-//test
