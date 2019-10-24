f9169aaf636ec8823508a0f3141f291e9b53f927
Laurie Frank <laurie360@hotmail.co.uk>
2019-10-24 15:05:20 +0100

    final commit

--- src/App.vue
+++ src/App.vue
@@ -13,48 +13,66 @@ export default {
   data(){
     return {
       data: {},
-      organisedData: []
+      data24: [],
+      organisedData: [],
+      organisedLineData: []
     }
   },
   components: {
     "energy-tracker": EnergyTracker
-      },
+  },
+
+
   mounted(){
     fetch('https://api.carbonintensity.org.uk/generation')
-    .then(response => response.json())
-    .then((fetchedData) => {
-      this.data = fetchedData.data.generationmix
-      let mappedObj = this.data.map((obj) => Object.values(obj))
-      mappedObj.unshift(["fuel", "%"]);
-      this.organisedData = mappedObj
-    }
-  )
-  }
+      .then(response => response.json())
+      .then((fetchedData) => {
+        this.data = fetchedData.data.generationmix
+        let mappedObj = this.data.map((obj) => Object.values(obj))
+        mappedObj.unshift(["fuel", "%"]);
+        this.organisedData = mappedObj
+        }
+      )
+    fetch('https://api.carbonintensity.org.uk/generation/2018-05-15T12:00Z/pt24h')
+      .then(response => response.json())
+      .then(data => this.data24 = data.data)
+
+  // .then((fetchedData) => {
+    // this.data24 = fetchedData.data.generationmix
+    // Object.values(obj)
+    // let mappedObj = this.data24.map((obj) => Object.values(obj))
+    // mappedObj.unshift(["fuel", "%"]);
+    // this.organisedLineData = mappedObj
+    // console.log("mapped24Obj", mappedObj);
+  // }
+// )
+
 }
-  </script>
+}
+</script>
 
-  <style lang="css" scoped>
-  </style>
-
-
-
+<style lang="css" scoped>
+</style>
 
 
-  // return {
-  //   "data":[
-  //     {
-  //       "from": "2018-01-20T12:00Z",
-  //       "to": "2018-01-20T12:30Z",
-  //       energyTypes: [
-  //         {"fuel": "Milk", "perc": 0.1},
-  //         {"fuel": 'Cheese', "perc": 2.2},
-  //         {"fuel": 'Beans', "perc": 17.9}
-  //       ],
-  //       chartOptions: {
-  //         chart: {
-  //           title: "Energy Types and Usages",
-  //           subtitle: "Milk, Cheese, Beans"
-  //         }
-  //       }
-  //     }]
-  //   };
+
+
+
+// return {
+//   "data":[
+//     {
+//       "from": "2018-01-20T12:00Z",
+//       "to": "2018-01-20T12:30Z",
+//       energyTypes: [
+//         {"fuel": "Milk", "perc": 0.1},
+//         {"fuel": 'Cheese', "perc": 2.2},
+//         {"fuel": 'Beans', "perc": 17.9}
+//       ],
+//       chartOptions: {
+//         chart: {
+//           title: "Energy Types and Usages",
+//           subtitle: "Milk, Cheese, Beans"
+//         }
+//       }
+//     }]
+//   };
